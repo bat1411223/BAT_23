1764c44b6351e0250bf8b93bd4e143059fb9c15d
patilm1240 <patilm1240@gmail.com>
2024-06-08 10:07:47 +0530

    BAT 23 Bdd project files

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,23 @@
+ package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "./Features/AddNewCust.feature",
+		glue="stepDefinations",
+		dryRun = false,
+		monochrome = true,
+		publish = true,
+		tags = "@Valid"
+	
+		
+		
+		
+		)
+public class TestRunner {
+
+}
